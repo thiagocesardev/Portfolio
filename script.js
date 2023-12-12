8eb2a4c5dfe515ce0d07ab5fd4c85416f41480ce
devthiagocesar <devthiagocesar@gmail.com>
2023-12-12 14:39:47 -0300

    121223..

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
-document.getElementById("btnDownload").addEventListener("click",function(){window.open("./assets/Curriculo_ThiagoCesar_FrontEnd.pdf")});
+document.getElementById("btnDownload").addEventListener("click",function()
+{window.open("./assets/Curriculo_ThiagoCesar_FrontEnd.pdf")});
