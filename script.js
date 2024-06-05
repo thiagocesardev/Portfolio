d38c040008352b73685de380ccc13b6a805f82d8
devthiagocesar <devthiagocesar@gmail.com>
2024-06-05 06:13:35 -0300

    230624

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getElementById("btnDownload").addEventListener("click", function() {
-    window.open("./assets/Curriculo_ThiagoCesar_Desenvolvedor.pdf");
+    window.open("./assets/Curriculo_Thiago_Cesar.pdf");
 });
